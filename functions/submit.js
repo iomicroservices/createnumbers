796be42bab518e7ad8f3c7e48fac6a42a11c96fc
britishno <info@britishnumbers.com>
2024-02-23 05:33:54 +0000

    t

--- functions/submit.js
+++ functions/submit.js
@@ -1,6 +1,16 @@
 export async function onRequestPost(context) {
     // Extracting the incoming request
     const { request, env } = context;
+
+    // Check the Origin header of the request
+    const origin = request.headers.get("Origin");
+    const allowedOrigins = "https://createnumbers.com, https://www.createnumbers.com, https://createnumbers.co.uk, https://www.createnumbers.co.uk";
+
+    // Return an error response if the origin is not as expected
+    if (!allowedOrigins.includes(origin)) {
+        return new Response("Unauthorized request", { status: 403 });
+    }
+
     const formData = await request.formData();
 
     // Append additional fields to formData
