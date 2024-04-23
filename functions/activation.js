dc6d041b03f6d723df75ddb9559fbb520a1da307
iomicroservices <106287942+iomicroservices@users.noreply.github.com>
2024-04-23 11:39:45 +0100

    Update activation.js

--- functions/activation.js
+++ functions/activation.js
@@ -1,34 +1,48 @@
 export async function onRequestPost(context) {
-    // Extracting the incoming request
     const { request, env } = context;
 
-    // Check the Origin header of the request
     const origin = request.headers.get("Origin");
     const allowedOrigins = "https://createnumbers.com, https://www.createnumbers.com, https://createnumbers.co.uk, https://www.createnumbers.co.uk";
 
-    // Return an error response if the origin is not as expected
     if (!allowedOrigins.includes(origin)) {
         return new Response("Unauthorized request", { status: 403 });
     }
 
-    // Convert the FormData to a JSON object
-    const formData = await request.formData();
-    const json = {};
-    formData.forEach((value, key) => { json[key] = value; });
-    json['source'] = 'Create'; // Append the additional field
+    // Assuming the body is JSON
+    const data = await request.json();
 
-    // Forward the form data as JSON to the REQUEST_URL
-    const response = await fetch(env.ACTIVATION_URL, {
+    // Prepare data for Airtable
+    const airtableData = {
+        "records": [
+            {
+                "fields": {
+                    "Email": data.email,
+                    "Code": data.code,
+                    "Source": data.source
+                }
+            }
+        ]
+    };
+
+    const airtableResponse = await fetch(env.AIRTABLE_URL, {
         method: 'POST',
-        body: JSON.stringify(json), // Convert the JSON object to a string
         headers: {
-            'Content-Type': 'application/json', // Set Content-Type to application/json
+            'Authorization': `Bearer ${env.AIRTABLE_API_KEY}`,
+            'Content-Type': 'application/json'
         },
+        body: JSON.stringify(airtableData)
     });
 
-    // Regardless of the response from REQUEST_URL, return a success message
+    if (!airtableResponse.ok) {
+        return new Response("Failed to post data to Airtable", {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
+
+    // Return success message
     return new Response(JSON.stringify({ message: "Form submitted successfully." }), {
         headers: { 'Content-Type': 'application/json' },
-        status: 200, // HTTP Status Code for OK
+        status: 200
     });
 }
